75811d68148a112dc66f0e4b7e7b68c826bb59df
Nikolas <nikolasbs11@gmail.com>
2025-07-17 15:10:57 -0300

    Added folders for all components and a global.css

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import './style.css'
+
+const Header = (() => {
+    return(
+        <header>
+            <p>Logo</p>
+
+            <nav class="header-links">
+                <ul class="header-items">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Portifolio</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+})
+
+export default Header
